8fc95f00b662d4cf3508c6e71fc108eacdb6960b
AlexProgrammerDE <abc123-50@gmx.de>
2020-01-09 21:04:04 +0100

    Add scp password feature

---
+++ scp-server/start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ -z "$SCP_PASSWORD" ]]; then
+  export SCP_PASSWORD=balenaserver
+fi
+
+# here we set up the config for openSSH.
+mkdir /var/run/sshd
+echo "root:$SCP_PASSWORD" | chpasswd
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+# SSH login fix. Otherwise user is kicked off after login
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
+export NOTVISIBLE="in users profile"
+echo "export VISIBLE=now" >> /etc/profile
+
+exec /usr/sbin/sshd -D
