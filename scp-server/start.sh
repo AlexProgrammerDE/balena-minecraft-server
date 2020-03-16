bb46d1396a35d3449357823e21cd30a1dbc2545c
Julien Larios <julien.larios@gmail.com>
2020-03-16 21:53:51 +0100

    Migrate SCP container to Balena image and fix SCP server issue

--- scp-server/start.sh
+++ scp-server/start.sh
@@ -7,10 +7,11 @@ fi
 # here we set up the config for openSSH.
 mkdir /var/run/sshd
 echo "root:$SCP_PASSWORD" | chpasswd
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
-# SSH login fix. Otherwise user is kicked off after login
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+# SSH login fix. Otherwise user is kicked off after login. 
+# Apparently not needed with balena image
+# sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 export NOTVISIBLE="in users profile"
 echo "export VISIBLE=now" >> /etc/profile
